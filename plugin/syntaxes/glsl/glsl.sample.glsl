d12f57ce8ccbb7b63ecdf0a44c0678cb4f7c27ed
sebthom <sebthom@users.noreply.github.com>
2023-07-28 16:47:34 +0200

    Add GLSL syntax. Fixes #59

---
+++ plugin/syntaxes/glsl/glsl.sample.glsl
@@ -0,0 +1,28 @@
+#version
+#version 460
+#version 460 core
+
+#import "/lib/test1"
+#import </lib/test1>
+
+#pragma optimize(on)
+#pragma debug(off)
+#pragma STDGL invariant(all)
+
+#extension GL_ARB_gpu_shader5 : enable
+
+#define DEF
+#undef DEF
+#if !defined SSAO && defined SSR
+
+layout (location = 0) in vec3 POSITION;
+
+uniform sampler2D colortex1;
+
+void SetVoxelTint(inout vec4[2] voxel, vec3 tint) {
+	voxel[0].rgb = tint;
+}
+
+void SetVoxelId(inout vec4[2] voxel, int id) {
+	voxel[0].a = 1.0 - id / 255.0;
+}
--- plugin/update-syntaxes.py
+++ plugin/update-syntaxes.py
@@ -16,34 +16,35 @@ THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 URL_PREFIX = "https://raw.githubusercontent.com/"
 
 syntaxes = (
-    ("apache-http", "mrmlnc/vscode-apache/master", "syntaxes/Apache.tmLanguage", "apache.configuration.json", "LICENSE"),
-    ("c", "microsoft/vscode/main", "extensions/cpp/syntaxes/c.tmLanguage.json", "extensions/cpp/language-configuration.json", "LICENSE.txt"),
-    ("cpp", "microsoft/vscode/main", "extensions/cpp/syntaxes/cpp.tmLanguage.json", "extensions/cpp/language-configuration.json", "LICENSE.txt"),
-    ("csharp", "microsoft/vscode/main", "extensions/csharp/syntaxes/csharp.tmLanguage.json", "extensions/csharp/language-configuration.json", "LICENSE.txt"),
-    ("dart", "microsoft/vscode/main", "extensions/dart/syntaxes/dart.tmLanguage.json", "extensions/dart/language-configuration.json", "LICENSE.txt"),
-    ("dot", "textmate/graphviz.tmbundle/master", "Syntaxes/DOT.plist", "", ""),
-    ("er", "mikkel-ol/vsc-er-syntax-highlighting/master", "syntaxes/er.tmLanguage.json", "language-configuration.json", "LICENSE"),
-    ("graphql", "graphql/vscode-graphql/master", "grammars/graphql.json", "language/language-configuration.json", "LICENSE"),
-    ("hcl", "hashicorp/syntax/main", "syntaxes/hcl.tmGrammar.json", "", "LICENSE"),
-    ("ini", "microsoft/vscode/main", "extensions/ini/syntaxes/ini.tmLanguage.json", "extensions/ini/ini.language-configuration.json", "LICENSE.txt"),
-    ("kotlin", "fwcd/kotlin-language-server/main", "grammars/Kotlin.tmLanguage.json", "grammars/kotlin.configuration.json", "LICENSE.txt"),
-    ("ldif", "FlashSystems/LDIF-Syntax/master", "ldif.tmLanguage", "", "LICENSE"),
-    ("lua", "microsoft/vscode/main", "extensions/lua/syntaxes/lua.tmLanguage.json", "extensions/lua/language-configuration.json", "LICENSE.txt"),
-    ("haxe", "vshaxe/haxe-TmLanguage/master", "haxe.tmLanguage", "../../vshaxe/master/configurations/haxe.language-configuration.json", "LICENSE.md"),
-    ("makefile", "microsoft/vscode/main", "extensions/make/syntaxes/make.tmLanguage.json", "extensions/make/language-configuration.json", "LICENSE.txt"),
-    ("php", "microsoft/vscode/main", "extensions/php/syntaxes/php.tmLanguage.json", "extensions/php/language-configuration.json", "LICENSE.txt"),
-    ("plantuml", "qjebbs/vscode-plantuml/master", "syntaxes/plantuml.yaml-tmLanguage", "language-configuration.json", "LICENSE.txt"),
-    ("powershell", "microsoft/vscode/main", "extensions/powershell/syntaxes/powershell.tmLanguage.json", "extensions/powershell/language-configuration.json", "LICENSE.txt"),
-    ("prisma", "prisma/language-tools/main", "packages/vscode/syntaxes/prisma.tmLanguage.json", "packages/vscode/language-configuration.json", "LICENSE"),
-    ("sentinel", "hashicorp/syntax/main", "syntaxes/sentinel.tmGrammar.json", "", "LICENSE"),
-    ("svelte", "sveltejs/language-tools/master", "packages/svelte-vscode/syntaxes/svelte.tmLanguage.src.yaml", "packages/svelte-vscode/language-configuration.json", "LICENSE"),
-    ("swift", "microsoft/vscode/main", "extensions/swift/syntaxes/swift.tmLanguage.json", "extensions/swift/language-configuration.json", "LICENSE.txt"),
-    ("terraform", "hashicorp/syntax/main", "syntaxes/terraform.tmGrammar.json", "", "LICENSE"),
-    ("toml", "oovm/vscode-toml/master", "extension/toml.tmLanguage.json", "extension/toml.configuration.json", "License.md"),
+    ("apache-http", "mrmlnc/vscode-apache/master", "syntaxes/Apache.tmLanguage", "apache.configuration.json", "LICENSE", ""),
+    ("c", "microsoft/vscode/main", "extensions/cpp/syntaxes/c.tmLanguage.json", "extensions/cpp/language-configuration.json", "LICENSE.txt", ""),
+    ("cpp", "microsoft/vscode/main", "extensions/cpp/syntaxes/cpp.tmLanguage.json", "extensions/cpp/language-configuration.json", "LICENSE.txt", ""),
+    ("csharp", "microsoft/vscode/main", "extensions/csharp/syntaxes/csharp.tmLanguage.json", "extensions/csharp/language-configuration.json", "LICENSE.txt", ""),
+    ("dart", "microsoft/vscode/main", "extensions/dart/syntaxes/dart.tmLanguage.json", "extensions/dart/language-configuration.json", "LICENSE.txt", ""),
+    ("dot", "textmate/graphviz.tmbundle/master", "Syntaxes/DOT.plist", "", "", ""),
+    ("er", "mikkel-ol/vsc-er-syntax-highlighting/master", "syntaxes/er.tmLanguage.json", "language-configuration.json", "LICENSE", ""),
+    ("glsl", "GeForceLegend/vscode-glsl/master", "syntaxes/glsl.tmLanguage.json", "language-configuration.json", "LICENSE", "sample/test.glsl"),
+    ("graphql", "graphql/vscode-graphql/master", "grammars/graphql.json", "language/language-configuration.json", "LICENSE", ""),
+    ("hcl", "hashicorp/syntax/main", "syntaxes/hcl.tmGrammar.json", "", "LICENSE", ""),
+    ("ini", "microsoft/vscode/main", "extensions/ini/syntaxes/ini.tmLanguage.json", "extensions/ini/ini.language-configuration.json", "LICENSE.txt", ""),
+    ("kotlin", "fwcd/kotlin-language-server/main", "grammars/Kotlin.tmLanguage.json", "grammars/kotlin.configuration.json", "LICENSE.txt", ""),
+    ("ldif", "FlashSystems/LDIF-Syntax/master", "ldif.tmLanguage", "", "LICENSE", ""),
+    ("lua", "microsoft/vscode/main", "extensions/lua/syntaxes/lua.tmLanguage.json", "extensions/lua/language-configuration.json", "LICENSE.txt", ""),
+    ("haxe", "vshaxe/haxe-TmLanguage/master", "haxe.tmLanguage", "../../vshaxe/master/configurations/haxe.language-configuration.json", "LICENSE.md", ""),
+    ("makefile", "microsoft/vscode/main", "extensions/make/syntaxes/make.tmLanguage.json", "extensions/make/language-configuration.json", "LICENSE.txt", ""),
+    ("php", "microsoft/vscode/main", "extensions/php/syntaxes/php.tmLanguage.json", "extensions/php/language-configuration.json", "LICENSE.txt", ""),
+    ("plantuml", "qjebbs/vscode-plantuml/master", "syntaxes/plantuml.yaml-tmLanguage", "language-configuration.json", "LICENSE.txt", ""),
+    ("powershell", "microsoft/vscode/main", "extensions/powershell/syntaxes/powershell.tmLanguage.json", "extensions/powershell/language-configuration.json", "LICENSE.txt", ""),
+    ("prisma", "prisma/language-tools/main", "packages/vscode/syntaxes/prisma.tmLanguage.json", "packages/vscode/language-configuration.json", "LICENSE", ""),
+    ("sentinel", "hashicorp/syntax/main", "syntaxes/sentinel.tmGrammar.json", "", "LICENSE", ""),
+    ("svelte", "sveltejs/language-tools/master", "packages/svelte-vscode/syntaxes/svelte.tmLanguage.src.yaml", "packages/svelte-vscode/language-configuration.json", "LICENSE", ""),
+    ("swift", "microsoft/vscode/main", "extensions/swift/syntaxes/swift.tmLanguage.json", "extensions/swift/language-configuration.json", "LICENSE.txt", ""),
+    ("terraform", "hashicorp/syntax/main", "syntaxes/terraform.tmGrammar.json", "", "LICENSE", ""),
+    ("toml", "oovm/vscode-toml/master", "extension/toml.tmLanguage.json", "extension/toml.configuration.json", "License.md", ""),
     # https://github.com/tamasfe/taplo/issues/245
-    # ("toml", "tamasfe/taplo/master", "editors/vscode/toml.tmLanguage.json", "editors/vscode/language-configuration.json", "LICENSE.md"),
-    ("vb", "microsoft/vscode/main", "extensions/vb/syntaxes/asp-vb-net.tmlanguage.json", "extensions/vb/language-configuration.json", "LICENSE.txt"),
-    ("vue", "vuejs/vue-syntax-highlight/master", "vue.tmLanguage", "", "LICENSE")
+    # ("toml", "tamasfe/taplo/master", "editors/vscode/toml.tmLanguage.json", "editors/vscode/language-configuration.json", "LICENSE.md", ""),
+    ("vb", "microsoft/vscode/main", "extensions/vb/syntaxes/asp-vb-net.tmlanguage.json", "extensions/vb/language-configuration.json", "LICENSE.txt", ""),
+    ("vue", "vuejs/vue-syntax-highlight/master", "vue.tmLanguage", "", "LICENSE", "")
 )
 
 DOWNLOAD_DIR = os.path.join(THIS_FILE_DIR, "syntaxes")
@@ -69,20 +70,20 @@ def download(github_branch, github_file, target_file):
         unix2dos(os.path.join(syntax_dir, target_file))
 
 
-for (lang_id, github_branch, syntax_uri, langcfg_uri, licence_uri)  in syntaxes:
+for (lang_id, github_branch, syntax_path, langcfg_path, license_path, sample_path)  in syntaxes:
     print(f"Downloading [{lang_id}] Grammar...")
     syntax_dir = os.path.join(DOWNLOAD_DIR, lang_id)
     os.makedirs(syntax_dir, exist_ok = True)
 
     # download in JSON format
-    if re.match(".*json.*", syntax_uri, re.IGNORECASE):
-        download(github_branch, syntax_uri, os.path.join(syntax_dir, f"{lang_id}.tmLanguage.json"))
+    if re.match(".*json.*", syntax_path, re.IGNORECASE):
+        download(github_branch, syntax_path, os.path.join(syntax_dir, f"{lang_id}.tmLanguage.json"))
 
     # download in YAML format and convert to JSON
-    elif re.match(".*yaml.*", syntax_uri, re.IGNORECASE):
+    elif re.match(".*yaml.*", syntax_path, re.IGNORECASE):
         yaml_file = os.path.join(syntax_dir, f"{lang_id}.tmLanguage.yaml")
         json_file = os.path.join(syntax_dir, f"{lang_id}.tmLanguage.json")
-        download(github_branch, syntax_uri, os.path.join(syntax_dir, yaml_file))
+        download(github_branch, syntax_path, os.path.join(syntax_dir, yaml_file))
 
         with open(yaml_file) as infile:
             yaml_content = yaml.load(infile, Loader = yaml.FullLoader)
@@ -94,14 +95,18 @@ for (lang_id, github_branch, syntax_uri, langcfg_uri, licence_uri)  in syntaxes:
 
     # download in PLIST/XML format
     else:
-        download(github_branch, syntax_uri, os.path.join(syntax_dir, f"{lang_id}.tmLanguage.plist"))
+        download(github_branch, syntax_path, os.path.join(syntax_dir, f"{lang_id}.tmLanguage.plist"))
 
-    if langcfg_uri:
+    if langcfg_path:
         print(f"Downloading [{lang_id}] Language Config...")
-        download(github_branch, langcfg_uri, os.path.join(syntax_dir, f"{lang_id}.langcfg.json"))
+        download(github_branch, langcfg_path, os.path.join(syntax_dir, f"{lang_id}.langcfg.json"))
 
-    if licence_uri:
+    if license_path:
         print(f"Downloading [{lang_id}] License...")
-        download(github_branch, licence_uri, os.path.join(syntax_dir, "LICENSE.txt"))
+        download(github_branch, license_path, os.path.join(syntax_dir, "LICENSE.txt"))
+
+    if sample_path:
+        print(f"Downloading [{lang_id}] Sample file...")
+        download(github_branch, sample_path, os.path.join(syntax_dir, f"{lang_id}.sample{os.path.splitext(sample_path)[1]}"))
 
 print("**DONE**")

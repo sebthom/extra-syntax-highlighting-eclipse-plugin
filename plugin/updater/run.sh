4e4dcd182cd210fd29f9b7d0f3e271b0ddf93cfa
sebthom <sebthom@users.noreply.github.com>
2025-06-24 16:35:21 +0200

    ci: update build workflow

--- plugin/updater/run.sh
+++ plugin/updater/run.sh
@@ -1,11 +1,12 @@
+#!/bin/sh
 # SPDX-FileCopyrightText: © Sebastian Thomschke and contributors.
 # SPDX-FileContributor: Sebastian Thomschke
 # SPDX-License-Identifier: EPL-2.0
 # SPDX-ArtifactOfProjectHomePage: https://github.com/sebthom/extra-syntax-highlighting-eclipse-plugin
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 
 export MAVEN_OPTS="-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -XX:TieredStopAtLevel=1 -XX:+UseParallelGC"
 
 echo "Launching..."
-mvn --quiet -e compiler:compile -Dexec.mainClass="updater.Updater" exec:java -Dexec.args="$@"
+mvn --quiet -e compiler:compile -Dexec.mainClass="updater.Updater" exec:java -Dexec.args="$*"
